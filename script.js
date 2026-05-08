07eba267a97ee4f295aa1307eb6a6af2f8b1d847
Akash <akashpillai40@gmail.com>
2026-05-08 13:56:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-//complete this code
-class Animal {}
+// Base Animal class
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
 
-class Dog extends Animal {}
+  // Getter for species
+  get species() {
+    return this._species;
+  }
 
-class Cat extends Animal {}
+  // Method to make a sound
+  makeSound() {
+    console.log(`The ${this._species} makes a sound`);
+  }
+}
 
-// Do not change the code below this line
-window.Animal = Animal;
-window.Dog = Dog;
-window.Cat = Cat;
+// Cat subclass
+class Cat extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Cat-specific behavior
+  purr() {
+    console.log("purr");
+  }
+}
+
+// Dog subclass
+class Dog extends Animal {
+  constructor(species) {
+    super(species);
+  }
+
+  // Dog-specific behavior
+  bark() {
+    console.log("woof");
+  }
+}
+
+// Example usage
+const myCat = new Cat("Siamese");
+myCat.makeSound(); // The Siamese makes a sound
+myCat.purr();      // purr
+
+const myDog = new Dog("Golden Retriever");
+myDog.makeSound(); // The Golden Retriever makes a sound
+myDog.bark();      // woof
+
